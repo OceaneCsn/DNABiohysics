5380487ac9019a3074e9e06a7eb8227ee2fbf36a
Oceane Csn <oceane.cassan@insa-lyon.fr>
2018-11-09 09:54:25 +0100

    del

--- Genome.py
+++ Genome.py
@@ -14,25 +14,27 @@ import random as rd
 class Genome():
 
 
-    def __init__(self, f=None, first_gen = False, indel_size = 60):
+    def __init__(self, f=None, first_gen = False, indel_size = 60, indelInvRatio = True):
         ## Creates a genome. If this is the first genome of an evolutive process,
         # the genome is initialized with the files in the folder pathToFiles
         # else, the genome will be initialized by copy later
         if first_gen:
-            #pathToFiles = '/home/ocassan/ProjetSimADN/'
-            pathToFiles = '/home/julie/Documents/5BIM/BacteriaEvolution/ProjetSimADN/'
+            pathToFiles = '/home/ocassan/ProjetSimADN/Init_files'
+            #pathToFiles = '/home/julie/Documents/5BIM/BacteriaEvolution/ProjetSimADN/'
             tmp = pd.read_csv(os.path.join(pathToFiles,'tousgenesidentiques.gff'), header = None, names =  ['name'])
             self.data = pd.DataFrame(tmp.name.str.split('\t',8).tolist(), columns = ['Seqname', 'Source', 'Feature', 'Start', 'End', 'Score', 'Strand', 'Frame' , 'Attribute'])
             self.genes = pd.DataFrame(self.data.iloc[4:len(self.data),:])
             self.genes = self.genes.reset_index(drop = True)
             self.size = int(self.genes['End'][0])
             self.header = self.data.iloc[0:4,:]
+            #rapport indels/inversions
             self.f = f
             self.TSS = pd.read_csv(os.path.join(pathToFiles,'TSS.dat'), sep =  '\t')
             self.TTS = pd.read_csv(os.path.join(pathToFiles,'TTS.dat'), sep =  '\t')
             self.barrier = pd.read_csv(os.path.join(pathToFiles,'prot.dat'), sep =  '\t')
             self.env = pd.read_csv(os.path.join(pathToFiles,'environment.dat'), header = None, sep =  '\t')
             self.indel_size = indel_size
+            self.indelInvRatio = indelInvRatio
 
 
     def copy(self, gen):
@@ -63,19 +65,71 @@ class Genome():
         for b in self.barrier['prot_pos']:
             genome[b] = 'b'
 
-        self.gen = np.array(list(genome))
+        
+        self.gen_ancetre = np.array(list(genome))
         return self.gen
 
 
     #def inversion(self):
+        #self.gen = np.array(self.gen_ancetre)
 
 
     def insertion(self):
+        self.gen = np.array(self.gen_ancetre)
+        
         intergenes = np.where(self.gen == 'i')[0]
         ins_pos = rd.choice(intergenes)
         print("Insertion at : ", ins_pos)
         self.gen = np.insert(self.gen, ins_pos, np.array(['i' for j in range(self.indel_size)]))
 
+    
+    def deletion(self):
+        
+        self.gen = np.array(self.gen_ancetre)
+        
+        intergenes = np.where(self.gen == 'i')[0]
+        for i in list(g0.barrier["prot_pos"])+list(g0.TSS["TSS_pos"]):
+            print(i)
+            if i<self.indel_size:
+                new_i = self.gen.size - (self.indel_size - i)
+                print("new i ", new_i)
+                
+                to_delete = []
+                to_delete2 = []
+                for inter in intergenes:
+                    if inter in range(new_i, self.gen.size):
+                        to_delete.append(inter)
+                    
+                intergenes = np.delete(intergenes, to_delete)
+                
+                for inter in intergenes:
+                    if inter in range(new_i, self.gen.size):
+                        to_delete2.append(inter)
+
+                intergenes = np.delete(intergenes, to_delete2)
+            else:
+                to_delete = []
+                for inter in intergenes:
+                    if inter in range(i-self.indel_size, i):
+                        to_delete.append(inter)
+                intergenes = np.delete(intergenes, to_delete)
+        del_pos = rd.choice(intergenes)
+        print("Deletion at : ", del_pos)
+
+        if(del_pos > self.gen.size - self.indel_size):
+            print("Positions deleted : ", range(del_pos, self.gen.size), " and ", range(0, self.indel_size - (self.gen.size - del_pos)))
+            self.gen = np.delete(self.gen, range(del_pos, self.gen.size))
+            self.gen = np.delete(self.gen, range(0, self.indel_size - (self.gen.size - del_pos)))
+        else:
+            print("Positions deleted : ", range(del_pos, del_pos + self.indel_size))
+            self.gen = np.delete(self.gen, range(del_pos, del_pos + self.indel_size))
+            
+            
+    def fitness(self):
+        pathToFiles = '/home/ocassan/ProjetSimADN/'
+        new_env = pd.read_csv(os.path.join(pathToFiles,'environment1.dat'), header = None, sep =  '\t')
+        return np.exp(-sum((new_env.iloc[:,1]-self.env.iloc[:,1])/self.env.iloc[:,1]))
+            
     def update_files(self):
         #update barriers
         self.barrier['prot_pos'] = np.where(self.gen == 'b')[0]
@@ -92,42 +146,46 @@ class Genome():
         self.genes.iloc[0,4] = self.gen.size
         self.genes.iloc[1:,3] = list(self.TSS["TSS_pos"])
         self.genes.iloc[1:,4] = list(self.TTS["TTS_pos"])
+        
+        self.data.iloc[4,4] = self.gen.size
+        self.data.iloc[5:,3] = list(self.TSS["TSS_pos"])
+        self.data.iloc[5:,4] = list(self.TTS["TTS_pos"])
 
 
-    def deletion(self):
-        intergenes = np.where(self.gen == 'i')[0]
-        for i in list(g0.barrier["prot_pos"])+list(g0.TSS["TSS_pos"]):
-            print(i)
-            if i<self.indel_size:
-                new_i = self.gen.size - (self.indel_size - i)
-                print("new i ", new_i)
-                intergenes = np.delete(intergenes, range(new_i, self.gen.size))
-#A mon avis, ce qu'on fait c'est pas okay : il faudrait prendre le génome copié et ensuite prendre les 'i' et delete ça car dans intergenes l'association index-position est faussée
-                intergenes = np.delete(intergenes, range(0, self.indel_size-i))
+    def dataframes_to_text(self):
+                    
+        self.TSS.to_csv('TSS.dat', header=True, index=False, sep='\t', mode='w')
+        self.TTS.to_csv('TTS.dat', header=True, index=False, sep='\t', mode='w')
+        self.barrier.to_csv('prot.dat', header=True, index=False, sep='\t', mode='w')
+        self.data.to_csv('tousgenesindentiques.gff', header=False, index=False, sep='\t', mode='w')
+        
+        
+        
+    def evolution(self, T):
+        
+        self.create_genome()
+        r = rd.random()
+        if(self.indelInvRatio):
+            if  r < self.f:
+            #indel
             else:
-                intergenes = np.delete(intergenes, range(i-self.indel_size, i))
-        del_pos = rd.choice(intergenes)
-        print("Deletion at : ", del_pos)
-
-        if(del_pos > self.gen.size - self.indel_size):
-            print("Positions deleted : ", range(del_pos, self.gen.size), " and ", range(0, self.indel_size - (self.gen.size - del_pos)))
-            self.gen = np.delete(self.gen, range(del_pos, self.gen.size))
-            self.gen = np.delete(self.gen, range(0, self.indel_size - (self.gen.size - del_pos)))
+                #inversion
         else:
-            print("Positions deleted : ", range(del_pos, del_pos + self.indel_size))
-            self.gen = np.delete(self.gen, range(del_pos, del_pos + self.indel_size))
+            if  r < self.f:
+                #inversion
+            else:
+                #indel
+        
+	
 
-	#def fitness(self):
 
 g0 = Genome(f = 0.5, first_gen=True)
 l = g0.create_genome()
-print(g0.barrier)
-#g0.insertion()
-g0.update_files()
-print(g0.barrier)
-print(g0.gen.shape)
+#i = np.where(l == 'i')[0]
 
-print(g0.genes)
-g0.deletion()
+#g0.deletion()
+g0.insertion()
 g0.update_files()
-print(g0.genes)
+g0.dataframes_to_text()
+g0.data
+n = pd.read_csv(os.path.join(pathToFiles,'environment1.dat'), header = None, sep =  '\t')
