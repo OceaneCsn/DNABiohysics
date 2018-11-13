9066a7bed868ca764818055b4fa0c6dba4776575
Julie Etienne <julie.etienne11@hotmail.fr>
2018-11-13 22:29:14 +0100

    Optimization deletion method

--- Genome.py
+++ Genome.py
@@ -21,9 +21,9 @@ class Genome():
         The genome is initialized with the files in the folder pathToFiles
         Mettre la signification de tous les attributs aussi
         '''
-        pathToFiles = 'D:/ProjetSimADN/Init_files'
+        #pathToFiles = 'D:/ProjetSimADN/Init_files'
         #pathToFiles = '/home/ocassan/ProjetSimADN/Init_files'
-        #pathToFiles = '/home/julie/Documents/5BIM/BacteriaEvolution/ProjetSimADN/'
+        pathToFiles = '/home/julie/Documents/5BIM/BacteriaEvolution/ProjetSimADN/Init_files/'
         tmp = pd.read_csv(os.path.join(pathToFiles,'tousgenesidentiques.gff'), header = None, names =  ['name'])
         self.data = pd.DataFrame(tmp.name.str.split('\t',8).tolist(), columns = ['Seqname', 'Source', 'Feature', 'Start', 'End', 'Score', 'Strand', 'Frame' , 'Attribute'])
         self.genes = pd.DataFrame(self.data.iloc[4:len(self.data),:])
@@ -63,20 +63,20 @@ class Genome():
 
     def inversion(self):
         self.gen = np.array(self.gen_ancetre)
-        
+
         #selection of two inversion bounds in intergenic region
         intergenes = np.where(self.gen == 'i')[0]
         pos1 = rd.choice(intergenes)
         pos2 = rd.choice(intergenes)
         print('inversion de ', min(pos1,pos2), ' a ', max(pos1,pos2))
-        
+
         #inversion des nucleotides
         inverted = np.flip(self.gen[min(pos1,pos2) : max(pos1, pos2)])
         print(inverted.size, inverted)
         list_gen = self.gen.tolist()
         list_gen[min(pos1,pos2) : max(pos1, pos2)] = inverted.tolist()
         self.gen = np.array(list_gen)
-        
+
         #changement du sens des genes dans le dataframe self.genes
         for g in self.genes_list:
             if g in self.gen[min(pos1,pos2) : max(pos1, pos2)]:
@@ -98,27 +98,72 @@ class Genome():
         print("Insertion at : ", ins_pos)
         self.gen = np.insert(self.gen, ins_pos, np.array(['i' for j in range(self.indel_size)]))
 
-    
+
     def deletion(self):
         '''
         randomly deletes a gene portion of size 60 bp, in the intergenic region
         and avoiding barrier proteins
         '''
         self.gen = np.array(self.gen_ancetre)
+        #Get all the non intergenes indices
+        not_intergenes = np.where(self.gen != 'i')[0]
+        genes_name = ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 'g10']
+        gene_length = 1000
+
+		#Only keep the beginning of the genes
+        get_genes_starts = []
+        for i in genes_name:
+            get_genes_starts.append(list(self.gen).index(i))
+        for i in get_genes_starts:
+            to_delete = [j for j in range(i + 1, i + gene_length)]
+            not_intergenes = not_intergenes[np.invert(np.isin(not_intergenes, to_delete))]
+        print(not_intergenes)
+
+		#Apply a protection field before each non-intergene element : between this number and the correpsonding one of not_intergenes, none can be deleted
+        to_protect_bounds = not_intergenes - self.indel_size
+        for i in to_protect_bounds:
+            if i < 0:
+                to_protect_bounds = np.append(to_protect_bounds, [self.gen.size + i, 0])
+                to_protect_bounds = to_protect_bounds[to_protect_bounds >= 0]
+        to_protect_bounds.sort()
+        print(to_protect_bounds)
+        not_possible = []
+        for i in zip(to_protect_bounds, not_intergenes):
+            not_possible.extend([j for j in range(i[0], i[1])])
+        for i in get_genes_starts:
+            not_possible.extend([j for j in range(i, i + gene_length)])
+        not_possible.sort()
+
+		#Set the indices where the del_pos can be
+        possible = np.array([i for i in range(0, self.gen.size)])
+        possible = possible[np.invert(np.isin(possible, not_possible))]
+        del_pos = rd.choice(possible)
+        print("del_pos = ", del_pos, " Is del_pos in not_possible ?", del_pos in not_possible)
+
+        #Remove
+        if(del_pos > self.gen.size - self.indel_size):
+            print("Positions deleted : ", range(del_pos, self.gen.size), " and ", range(0, self.indel_size - (self.gen.size - del_pos)))
+            self.gen = np.delete(self.gen, range(del_pos, self.gen.size))
+            self.gen = np.delete(self.gen, range(0, self.indel_size - (self.gen.size - del_pos)))
+        else:
+            print("Positions deleted : ", range(del_pos, del_pos + self.indel_size))
+            self.gen = np.delete(self.gen, range(del_pos, del_pos + self.indel_size))
+
+        '''
         intergenes = np.where(self.gen == 'i')[0]
         for i in list(g0.barrier["prot_pos"])+list(g0.TSS["TSS_pos"]):
             #print(i)
             if i<self.indel_size:
                 new_i = self.gen.size - (self.indel_size - i)
-                
+
                 to_delete = []
                 to_delete2 = []
                 for inter in intergenes:
                     if inter in range(new_i, self.gen.size):
                         to_delete.append(inter)
-                    
+
                 intergenes = np.delete(intergenes, to_delete)
-                
+
                 for inter in intergenes:
                     if inter in range(new_i, self.gen.size):
                         to_delete2.append(inter)
@@ -140,16 +185,19 @@ class Genome():
         else:
             print("Positions deleted : ", range(del_pos, del_pos + self.indel_size))
             self.gen = np.delete(self.gen, range(del_pos, del_pos + self.indel_size))
-            
+        '''
+
         return self.gen
+
     def compute_fitness(self, init = False):
         #pathToFiles = '/home/ocassan/ProjetSimADN/'
-        pathToFiles = 'D:/ProjetSimADN'
+        pathToFiles = '/home/julie/Documents/5BIM/BacteriaEvolution/ProjetSimADN/'
+        #pathToFiles = 'D:/ProjetSimADN'
         if init:
             pathToFiles = os.path.join(pathToFiles, 'Init_files')
         new_env = pd.read_csv(os.path.join(pathToFiles,'environment.dat'), header = None, sep =  '\t')
         return np.exp(-sum((new_env.iloc[:,1]-self.env.iloc[:,1])/self.env.iloc[:,1]))
-            
+
     def update_files(self):
         #update barriers positions
         self.barrier['prot_pos'] = np.where(self.gen_ancetre == 'b')[0]
@@ -172,17 +220,17 @@ class Genome():
         self.genes.iloc[0,4] = self.gen_ancetre.size
         self.genes.iloc[1:,3] = list(self.TSS["TSS_pos"])
         self.genes.iloc[1:,4] = list(self.TTS["TTS_pos"])
-        
+
         #orientation dans TSS et TTS
         self.TSS['TUorient'] = list(self.genes['Strand'].iloc[1:,])
         self.TTS['TUorient'] = list(self.genes['Strand'].iloc[1:,])
-        
+
         #tout mis a jour dans data
         self.data.iloc[4,4] = self.gen_ancetre.size
         self.data.iloc[5:,3] = list(self.TSS["TSS_pos"])
         self.data.iloc[5:,4] = list(self.TTS["TTS_pos"])
         self.data['Strand'].iloc[5:,] = list(self.genes['Strand'].iloc[1:,])
-        
+
     def dataframes_to_text(self):
         '''
         converts the dataframe attributes to .dat files that can be
@@ -192,11 +240,11 @@ class Genome():
         self.TTS.to_csv('TTS.dat', header=True, index=False, sep='\t', mode='w')
         self.barrier.to_csv('prot.dat', header=True, index=False, sep='\t', mode='w')
         self.data.to_csv('tousgenesindentiques.gff', header=False, index=False, sep='\t', mode='w')
-        
-        
-        
+
+
+
     def evolution_step(self, t):
-        
+
         #mutation du genome suivant la probabilite relative
         r = rd.random()
         if(self.indelInvRatio):
@@ -215,7 +263,7 @@ class Genome():
                     self.insertion()
                 else:
                     self.deletion()
-                    
+
         #evaluation de la fitness du nouveau genome par simulation
         '''
         ICI : utiliser le code du prof de github qui va generer le nouvel
@@ -238,8 +286,8 @@ class Genome():
             self.fitness = new_fitness
             self.update_files()
             self.dataframes_to_text()
-        
-                
+
+
     def evolution(self, T):
         '''
         simulates a genome evolution using a Monte Carlo algorithm
@@ -250,14 +298,18 @@ class Genome():
 
 
 g0 = Genome(f = 0.5)
-#g0.create_genome()
+g0.create_genome()
 #g0.evolution_step(0)
-g0.evolution(10)
+#g0.evolution(10)
 #de = g0.deletion()
+
+g0.deletion()
+g0.update_files()
+print(g0.gen.size)
+
 '''g0.create_genome()
 g0.inversion()
 l = g0.gen
 g0.genome_ancetre = l
 
 g0.update_files()'''
-
